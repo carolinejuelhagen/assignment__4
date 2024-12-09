8106d8aa8a39f2644f67a3ecc467f12acaeb6818
Caroline Juel Hagen <caroline.juel.hagen@outlook.com>
2024-12-10 00:46:55 +0100

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,21 @@
+// CALLINGS
+const sideBarLinks = document.querySelectorAll(".sidebar__link");
+console.log(sideBarLinks);
+
+const contentContainers = document.querySelectorAll(".content-container");
+
+console.log(contentContainers);
+
+// LOOP
+sideBarLinks.forEach((sidebarLink, index) => {
+	sidebarLink.addEventListener("click", (e) => {
+		contentContainers.forEach((contentContainer) => {
+			contentContainer.classList.remove("content-container--active");
+		});
+		sideBarLinks.forEach((link) => {
+			link.classList.remove("sidebar__link--active");
+		});
+		e.target.classList.add("sidebar__link--active");
+		contentContainers[index].classList.add("content-container--active");
+	});
+});
